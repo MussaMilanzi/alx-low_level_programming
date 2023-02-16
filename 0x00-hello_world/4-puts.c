c46cc3167f716d6c8bc7baa9f97b1c08c3c6f7da
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-16 15:43:21 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Code that prints a line using puts fuction
+ *Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
