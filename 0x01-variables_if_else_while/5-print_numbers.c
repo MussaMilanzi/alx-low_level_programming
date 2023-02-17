0ba194ab09530308530763d984a1a71bdf399747
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-17 13:00:30 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Program that prints number fron 0 to 9
+ * Retrun: Always 0
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i = < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
