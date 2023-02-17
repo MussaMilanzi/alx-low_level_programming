87bb47af80d362041e938fb7e3cf5ce0c98c0a7f
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-17 13:40:10 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all numbers
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i;
+	char x;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	for (x = 'a'; x = 'f'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
