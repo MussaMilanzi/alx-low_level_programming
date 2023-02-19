3fd67a8f84f08b2681e5cab41042d94975146a6f
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-02-19 13:29:15 -0800

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			if (i == j)
 				continue;
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-				putchar(' ');
-				putchar(i / 10 + '0');
-				putchar(j % 10 + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 			if (i != 99 || j != 98)
 			{
 				putchar(',');
