0baf71bd1954a958855ce7d4ab4d60d22f1b0926
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-23 13:52:06 -0800

    Task 5 correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,13 @@ void more_numbers(void)
 {
 	int count, i;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 1; count <= 10; count++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
-			{
 				_putchar(1);
-			}
-			_putchar(i % 2 == 0);
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
