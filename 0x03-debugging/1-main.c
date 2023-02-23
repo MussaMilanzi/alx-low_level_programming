a3dcc4258bd9c1487d00a0d79d1ec4429b20bdae
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-02-23 06:12:51 -0800

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 int i;
+  
 printf("Infinite loop incoming :(\n");
 
 i = 0;
@@ -16,6 +17,7 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
+  
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
