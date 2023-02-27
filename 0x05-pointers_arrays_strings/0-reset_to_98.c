4eae1e45ac61ec25a504a1a22cf787a2b0ad61f6
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-27 03:06:10 -0800

    Task 0 c pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Modifies the fuct value
+ *@n : value to be modified
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+The project is about C pointers, arrays, and  strings
+
+Task 0: function that takes a pointer to an int as parameter and updates the value it points to to 98.
