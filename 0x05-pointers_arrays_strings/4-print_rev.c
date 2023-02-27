62046131b1a8e3d6c8cdd48aecfd9584863e4e93
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-02-27 06:57:31 -0800

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,8 @@ void print_rev(char *s)
 	int len = 0;
 
 	while (s[len] != '\0')
-	{
 		len++;
-		for (len = len - 1; len >= 0; len--)
-			_putchar(s[len]);
-		_putchar('\n');
-	}
+	for (len = len - 1; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
 }
