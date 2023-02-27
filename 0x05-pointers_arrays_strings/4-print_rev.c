97d09f7db820c29449cf5fdb90ef969b81e23f25
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-02-27 06:38:57 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Print reverse rder string
+ * @s: vars s
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+		for (len = len - 1; len >= 0; len--)
+			_putchar(s[len]);
+		_putchar('\n');
+	}
+}
