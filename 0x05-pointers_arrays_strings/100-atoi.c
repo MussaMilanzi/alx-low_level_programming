fe35b65f4303e0ef2e7edf65bac40123fd6f5437
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-02-27 18:14:31 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,57 +8,33 @@
  */
 int _atoi(char *s)
 {
-	/**int result = 0;
-	int sign = 1;
-	int i = 0;
+int index, ind2;
+unsigned int res;
+int sign = 1;
+char now;
 
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-	return (sign * result);
-	*/
-	int index, ind2;
-	unsigned int res;
-	int sign = 1;
-	char now;
-
-	index = 0;
-	res = 0;
-	while (*(s + index) != '\0')
-	{
-		now = *(s + index);
-		if (now == '-')
-		{
-			sign *= -1;
-		}
-		if (now >= '0' && now <= '9')
-		{
-			ind2 = index;
-			while (*(s + ind2) > 47 && *(s + ind2) < 58)
-			{
-				res = (res * 10) + *(s + ind2) - '0';
-				ind2++;
-			}
-			break;
-		}
-		index++;
-	}
-	if (sign < 0)
-		res *= sign;
-	return (res);
+index = 0;
+res = 0;
+while (*(s + index) != '\0')
+{
+now = *(s + index);
+if (now == '-')
+{
+sign *= -1;
+}
+if (now >= '0' && now <= '9')
+{
+ind2 = index;
+while (*(s + ind2) > 47 && *(s + ind2) < 58)
+{
+res = (res * 10) + *(s + ind2) - '0';
+ind2++;
+}
+break;
+}
+index++;
+}
+if (sign < 0)
+res *= sign;
+return (res);
 }
