5e8b651a5b117d4008d22a5ebbd61c10427fee2e
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-03-10 06:58:47 -0800

    Task 0

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+
+/**
+ * _strcmp -  function that compares two strings.
+ * @s1 : pointerto char params
+ * @s2 : pointer to char params
+ * Return: *dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int n;
+
+	i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	n = s1[i] - s2[i];
+	return (n);
+}
