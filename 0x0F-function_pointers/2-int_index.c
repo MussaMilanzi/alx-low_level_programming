b838f7f0ea16afce32cd590594b40d31b93ce7dd
MussaMilanzi <milanzi.amandamussa@gmail.com>
2023-03-22 11:14:05 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Function thta searches for integer
+ * @array: array that points to elements in aarray
+ * @size: array length
+ * @cmp: pointer to a function to be used to
+ * compare values
+ *
+ * Return : 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
