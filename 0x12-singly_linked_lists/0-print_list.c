6c9dc121749e8c59c8d813bbc2ff65217bf16338
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-03-30 21:00:02 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0,  "(nil)");
-		}
-		else
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		i++;
 	}
