9396a186832ae8ea8686a7c64d76ac47aa336235
Mussa Milanzi <milanzi.amandamussa@gmail.com>
2023-04-10 13:42:41 -0700

    FIX: lopping

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fild;
-	ssize_t result_w;
-
-	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int fild, i;
+	int result_w;
 
 	if (filename == NULL)
 		return (-1);
 
+	fild = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fild == -1)
+		return (-1);
+
 	if (text_content == NULL)
-		result_w = write(fild, text_content, strlen(text_content));
+		text_content = "";
+
+	i = 0;
+	while (text_content[i])
+	{
+		i++;
+	}
+
+	result_w = write(fild, text_content, i);
+
 	if (result_w == -1)
 		return (-1);
+
 	close(fild);
+
 	return (1);
 }
